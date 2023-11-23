0e91ff61e2788d085801adb8f5614f1201d4f34f
jeongho77 <115057094+jeongho77@users.noreply.github.com>
2023-11-23 23:25:00 +0900

    Add files via upload

---
+++ 9week/company/index.js
@@ -0,0 +1,15 @@
+//네비게이션 버튼 클릭시 화면 이동
+function moveMain() {
+    location.href = "index.html"
+}
+function movecommunity() {
+    location.href = "community.html"
+}
+
+function moveintro() {
+    location.href = "comintro.html"
+}
+
+function moveactivities() {
+    location.href = "activity.html"
+}
